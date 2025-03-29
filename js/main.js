35f333f0786ba116c4e28a36f65105d563c9bf46
Deepak Dewatwal <136556140+Dewatwal@users.noreply.github.com>
2025-03-29 14:51:36 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const navLinks = document.querySelector('.nav-links');
 burger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+const tg_remove = document.querySelectorAll(".tg_remove");
+tg_remove.forEach((tg)=>{
+  tg.addEventListener("click",()=>{
+    navLinks.classList.toggle('active')
+  })
+})
