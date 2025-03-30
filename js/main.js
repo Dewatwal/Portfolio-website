82362ec3a793f3b1644b8c7289b87459f5b2b1e6
Deepak Dewatwal <136556140+Dewatwal@users.noreply.github.com>
2025-03-30 13:58:00 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -11,3 +11,29 @@ tg_remove.forEach((tg)=>{
     navLinks.classList.toggle('active')
   })
 })
+
+const linkedin = document.querySelector("#linkedin");
+console.log(linkedin);
+linkedin.addEventListener("click",()=>{
+  window.open('https://www.linkedin.com/feed/');
+})
+const github = document.querySelector("#github");
+console.log(linkedin);
+github.addEventListener("click",()=>{
+  window.open('https://github.com/');
+})
+const twitter = document.querySelector("#twitter");
+console.log(linkedin);
+twitter.addEventListener("click",()=>{
+  window.open('https://x.com/home');
+})
+const leetcode  = document.querySelector("#leetcode");
+console.log(linkedin);
+leetcode.addEventListener("click",()=>{
+  window.open('https://leetcode.com/u/Deepak-8/');
+})
+const instagram = document.querySelector("#instagram");
+console.log(linkedin);
+instagram.addEventListener("click",()=>{
+  window.open('https://www.instagram.com/deepak__prajapat08');
+})
